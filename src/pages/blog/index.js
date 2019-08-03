35fe7f740809a6d30fdacc60dbe3400f7830f441
Malte <skyguardian42@gmail.com>
2019-08-03 14:23:24 +0200

    Blog sort order reversed

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -9,6 +9,7 @@ export default () => {
   const data = useStaticQuery(graphql`
     query {
       allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] },
         filter: {
           frontmatter:
             {templateKey: {eq: "blog-post"}}
